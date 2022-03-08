a178f3120d22ec38ef22ffb770b3a440862c070e
ruturajnawale10 <ruturajnawale99@gmail.com>
2022-03-08 01:19:56 -0800

    Added register and login pages. Also connected Amazon RDS database and accessed in node JS.

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 //use express session to maintain session data
 app.use(session({
-    secret              : 'cmpe273_kafka_passport_mongo',
+    secret              : 'etsy-application',
     resave              : false, // Forces the session to be saved back to the session store, even if the session was never modified during the request
     saveUninitialized   : false, // Force to save uninitialized session to db. A session is uninitialized when it is new but not modified.
     duration            : 60 * 60 * 1000,    // Overall duration of Session : 30 minutes : 1800 seconds
@@ -31,17 +31,6 @@ app.use(function(req, res, next) {
     res.setHeader('Cache-Control', 'no-cache');
     next();
   });
-
-  var Users = [{
-      username : "admin",
-      password : "admin"
-  }]
-
-  var books = [
-    {"BookID" : "1", "Title" : "Book 1", "Author" : "Author 1"},
-    {"BookID" : "2", "Title" : "Book 2", "Author" : "Author 2"},
-    {"BookID" : "3", "Title" : "Book 3", "Author" : "Author 3"}
-]
 
 //create Amazon RDS mysql connection
 var con = mysql.createConnection({
@@ -97,57 +86,11 @@ app.post('/register',function(req,res){
     // var username = req.body.username;
     // var password = req.body.password;
     console.log("Inside Register Post Request");
-    //console.log("Req Body : ", username + "password : ",password);
     console.log("Req Body : ",req.body);
     Users.filter(function(user){
         res.end("Successful Registration"); 
     })
 });
-
-// //Route to get All Books when user visits the Home Page
-// app.get('/home', function(req,res){
-//     console.log("Inside Home Login");    
-//     res.writeHead(200,{
-//         'Content-Type' : 'application/json'
-//     });
-//     console.log("Books : ",JSON.stringify(books));
-//     res.end(JSON.stringify(books));
-    
-// })
-
-// app.post('/create',function(req, res) {
-//     console.log("Inside create");    
-//     res.writeHead(200,{
-//         'Content-Type' : 'application/json'
-//     });
-//     console.log("Books body", req.body);
-//     if (books.some(book => book.BookID === req.body.BookID)) {
-//         console.log('Cannot add the book. Entered BookID already exists!');
-//         res.end("book_addition_failed");
-//     }
-//     else {
-//         books.push({"BookID": req.body.BookID, "Title": req.body.Title, "Author": req.body.Author});
-//         res.end("book_addition_successful");
-//     }
-// })
-
-// app.post('/delete',function(req, res) {
-//     console.log("Inside delete");    
-//     res.writeHead(200,{
-//         'Content-Type' : 'application/json'
-//     });
-//     console.log("Books body", req.body);
-//     let previous_len = books.length;
-//     books = books.filter(book => book.BookID !== req.body.BookID);
-//     let curr_len = books.length;
-
-//     if (curr_len < previous_len) {
-//         res.end("deletion successful");
-//     }
-//     else {
-//         res.end("deletion failed");
-//     }    
-// })
 
 //start your server on port 3001
 app.listen(3001);
