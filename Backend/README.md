65309f0adbf832c8caf32cc0cb4b3fe54c0c5a49
ruturajnawale10 <ruturajnawale99@gmail.com>
2022-03-07 16:15:41 -0800

    Added node and express in backend folder

---
+++ Backend/README.md
@@ -0,0 +1,4 @@
+## To create node application and add express in the backend:
+### npm init
+### add: "start": "node index.js" in scripts in the package.json file
+### npm install express
---
+++ Backend/index.js
@@ -0,0 +1,7 @@
+//import the require dependencies
+import express from 'express';
+var app = express();
+
+//start your server on port 3001
+app.listen(3001);
+console.log("Server Listening on port 3001");
