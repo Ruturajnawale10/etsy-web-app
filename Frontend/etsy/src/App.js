083ee98d647f4f8fb76b7365f1388d00a7bb9eac
ruturajnawale10 <ruturajnawale99@gmail.com>
2022-03-08 22:16:14 -0800

    Added navigation bar in dashboard

--- Frontend/etsy/src/App.js
+++ Frontend/etsy/src/App.js
@@ -1,17 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import Main from './components/Main';
-import {BrowserRouter} from 'react-router-dom';
+import React, {Component} from 'react';
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+import { BrowserRouter as Router, Redirect, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     //Use Browser Router to route to different pages
-    <BrowserRouter>
-      <div>
-        {/* App Component Has a Child Component called Main*/}
-        <Main/>
+    <Router>
+    <div>
+      <Navbar/>
+        <Switch>
+              <Route exact path="/" component={Dashboard}/>
+              <Route path="/login" component={Login}/>
+              <Route path="/register" component={Register}/>
+              <Redirect from='*' to='/' />    {/*redirects any other url other then above to / path i.e. login*/}
+        </Switch>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
